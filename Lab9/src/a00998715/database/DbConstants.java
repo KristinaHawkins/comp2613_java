01261c5b51df7b91ad1c31dfd3e60e41f63c66dc
Edgar <edz@edgars-mbp.wifi.bcit.ca>
2017-03-10 02:02:26 -0800

    Lab9

---
+++ Lab9/src/a00998715/database/DbConstants.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package a00998715.database;
+
+/**
+ * @author scirka
+ *
+ */
+public interface DbConstants {
+
+	String DB_PROPERTIES_FILENAME = "db.properties";
+	String DB_DRIVER_KEY = "db.driver";
+	String DB_URL_KEY = "db.url";
+	String DB_USER_KEY = "db.user";
+	String DB_PASSWORD_KEY = "db.password";
+	String TABLE_ROOT = "S715_";
+	String CUSTOMER_TABLE_NAME = TABLE_ROOT + "Customer";
+
+}
